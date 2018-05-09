5554b7246b7d8e36c0d3d383c77f6626aff409d4
maud gennart <maud.gennart@imio.be>
2018-05-09 16:25:30 +0200

    Back to development: 0.2.13

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.memory package."""
 
-version = '0.2.12'
+version = '0.2.13.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
